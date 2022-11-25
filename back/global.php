8d225eb314e2a78c66ebd89952a6340960db270e
NayelliSZ <nayellisotoz@gmail.com>
2022-11-24 22:46:01 -0600

    Back
    Modificación al documento de API, y a los diagramas y script

--- back/global.php
+++ back/global.php
@@ -1,14 +1,18 @@
 <?php
+//creado por Nayelli el 17 de noviembre
+
 //Definir Servidor
-define("DB_HOST", "v2ray.dosedgames.tk");
+define("DB_HOST", "localhost");
 
 //Define nombre de BD
-define("DB_NAME","helpdesk");
+define("DB_NAME","help");
 
 //usuario bd 
-define("DB_USERNAME","Pichardo");
+define("DB_USERNAME","root");
 
 //password
-define("DB_PASSWORD","pichardo121289");
+define("DB_PASSWORD","");
+
+
 
 ?>
