6fbc82c0f8b0be3a238cc9d8edd255d88fbe4442
NayelliSZ <nayellisotoz@gmail.com>
2022-12-09 08:26:14 -0600

    Back
    Api (consulta por atributo)

--- api/apihelp.php
+++ api/apihelp.php
@@ -17,7 +17,157 @@
 						'nombre_cliente' => $res[$row][2],
 						'fecha_creacion' => $res[$row][3],
 						'fecha_solucion' => $res[$row][4],
-						'estatus' => ($res[$row][5]==0)? 'PENDIDNTE' : 'REALIZADO'
+						'estatus' => ($res[$row][5]==0)? 'PENDIENTE' : 'SOLUCIONADO'
+					);
+					array_push($tickets['items'], $item);
+				}
+				echo json_encode($tickets);
+			}else{
+				echo json_encode(array('mensaje' => 'No hay elementos registrados'));
+			}
+		}
+
+		function getId($folio){
+			$ticket = new help();
+			$tickets = array();
+			$tickets ["items"] = array();
+
+			$res =$ticket->obtenerTicketConsulta($folio);
+
+			if(count($res)>0){
+				for($row=0; $row<count($res); $row++){
+					$item = array(	
+						'folio' => $res[$row][0],
+						'id_empleado' => $res[$row][1],
+						'nombre_cliente' => $res[$row][2],
+						'fecha_creacion' => $res[$row][3],
+						'fecha_solucion' => $res[$row][4],
+						'estatus' => ($res[$row][5]==0)? 'PENDIENTE' : 'SOLUCIONADO'
+					);
+					array_push($tickets['items'], $item);
+				}
+				echo json_encode($tickets);
+			}else{
+				echo json_encode(array('mensaje' => 'No hay elementos registrados'));
+			}
+		}
+
+		function getEmpleado($id_empleado){
+			$ticket = new help();
+			$tickets = array();
+			$tickets ["items"] = array();
+
+			$res =$ticket->obtenerTicketEmpleado($id_empleado);
+
+			if(count($res)>0){
+				for($row=0; $row<count($res); $row++){
+					$item = array(	
+						'folio' => $res[$row][0],
+						'id_empleado' => $res[$row][1],
+						'nombre_cliente' => $res[$row][2],
+						'fecha_creacion' => $res[$row][3],
+						'fecha_solucion' => $res[$row][4],
+						'estatus' => ($res[$row][5]==0)? 'PENDIENTE' : 'SOLUCIONADO'
+					);
+					array_push($tickets['items'], $item);
+				}
+				echo json_encode($tickets);
+			}else{
+				echo json_encode(array('mensaje' => 'No hay elementos registrados'));
+			}
+		}
+
+		function getCliente($nombre_cliente){
+			$ticket = new help();
+			$tickets = array();
+			$tickets ["items"] = array();
+
+			$res =$ticket->obtenerTicketCliente($nombre_cliente);
+
+			if(count($res)>0){
+				for($row=0; $row<count($res); $row++){
+					$item = array(	
+						'folio' => $res[$row][0],
+						'id_empleado' => $res[$row][1],
+						'nombre_cliente' => $res[$row][2],
+						'fecha_creacion' => $res[$row][3],
+						'fecha_solucion' => $res[$row][4],
+						'estatus' => ($res[$row][5]==0)? 'PENDIENTE' : 'SOLUCIONADO'
+					);
+					array_push($tickets['items'], $item);
+				}
+				echo json_encode($tickets);
+			}else{
+				echo json_encode(array('mensaje' => 'No hay elementos registrados'));
+			}
+		}
+
+		function getCreacion($fecha_creacion){
+			$ticket = new help();
+			$tickets = array();
+			$tickets ["items"] = array();
+
+			$res =$ticket->obtenerTicketCreacion($fecha_creacion);
+
+			if(count($res)>0){
+				for($row=0; $row<count($res); $row++){
+					$item = array(	
+						'folio' => $res[$row][0],
+						'id_empleado' => $res[$row][1],
+						'nombre_cliente' => $res[$row][2],
+						'fecha_creacion' => $res[$row][3],
+						'fecha_solucion' => $res[$row][4],
+						'estatus' => ($res[$row][5]==0)? 'PENDIENTE' : 'SOLUCIONADO'
+					);
+					array_push($tickets['items'], $item);
+				}
+				echo json_encode($tickets);
+			}else{
+				echo json_encode(array('mensaje' => 'No hay elementos registrados'));
+			}
+		}
+
+		function getSolucion($fecha_solucion){
+			$ticket = new help();
+			$tickets = array();
+			$tickets ["items"] = array();
+
+			$res =$ticket->obtenerTicketSolucion($fecha_solucion);
+
+			if(count($res)>0){
+				for($row=0; $row<count($res); $row++){
+					$item = array(	
+						'folio' => $res[$row][0],
+						'id_empleado' => $res[$row][1],
+						'nombre_cliente' => $res[$row][2],
+						'fecha_creacion' => $res[$row][3],
+						'fecha_solucion' => $res[$row][4],
+						'estatus' => ($res[$row][5]==0)? 'PENDIENTE' : 'SOLUCIONADO'
+					);
+					array_push($tickets['items'], $item);
+				}
+				echo json_encode($tickets);
+			}else{
+				echo json_encode(array('mensaje' => 'No hay elementos registrados'));
+			}
+		}
+
+		function getEstatus($estatus){
+			$ticket = new help();
+			$tickets = array();
+			$tickets ["items"] = array();
+
+			$res =$ticket->obtenerTicketEstatus($estatus);
+
+			if(count($res)>0){
+				for($row=0; $row<count($res); $row++){
+					$item = array(	
+						'folio' => $res[$row][0],
+						'id_empleado' => $res[$row][1],
+						'nombre_cliente' => $res[$row][2],
+						'fecha_creacion' => $res[$row][3],
+						'fecha_solucion' => $res[$row][4],
+						'estatus' => ($res[$row][5]==0)? 'PENDIENTE' : 'SOLUCIONADO'
 					);
 					array_push($tickets['items'], $item);
 				}
--- api/help.php
+++ api/help.php
@@ -8,6 +8,47 @@
 
 			return ejecutarConsultaSimpleFila($query);
 		}
+
+		function obtenerTicketConsulta($id){
+			$query = "SELECT t.folio, t.id_empleado, c.nombre, t.fecha_creacion, t.fecha_solucion, t.estatus FROM tickets t, clientes c where c.id_clientes = t.id_cliente and t.folio=".$id;
+
+			return ejecutarConsultaSimpleFila($query);
+		}
+
+		function obtenerTicketEmpleado($empleado){
+			$query = "SELECT t.folio, t.id_empleado, c.nombre, t.fecha_creacion, t.fecha_solucion, t.estatus FROM tickets t, clientes c where c.id_clientes = t.id_cliente and t.id_empleado=".$empleado;
+
+			return ejecutarConsultaSimpleFila($query);
+		}
+
+		function obtenerTicketCliente($cliente){
+			$query = "SELECT t.folio, t.id_empleado, c.nombre, t.fecha_creacion, t.fecha_solucion, t.estatus FROM tickets t, clientes c where c.id_clientes = t.id_cliente and c.nombre=".$cliente;
+
+			return ejecutarConsultaSimpleFila($query);
+		}
+
+		function obtenerTicketCreacion($creacion){
+			$query = "SELECT t.folio, t.id_empleado, c.nombre, t.fecha_creacion, t.fecha_solucion, t.estatus FROM tickets t, clientes c where c.id_clientes = t.id_cliente and t.fecha_creacion=".$creacion;
+
+			return ejecutarConsultaSimpleFila($query);
+		}
+
+		function obtenerTicketSolucion($solucion){
+			$query = "SELECT t.folio, t.id_empleado, c.nombre, t.fecha_creacion, t.fecha_solucion, t.estatus FROM tickets t, clientes c where c.id_clientes = t.id_cliente and t.fecha_solucion=".$solucion;
+
+			return ejecutarConsultaSimpleFila($query);
+		}
+
+		function obtenerTicketEstatus($estatus){
+			if($estatus=="PENDIENTE"){
+				$estatus=0;
+			}else if($estatus=="SOLUCIONADO"){
+				$estatus=1;
+			}
+			$query = "SELECT t.folio, t.id_empleado, c.nombre, t.fecha_creacion, t.fecha_solucion, t.estatus FROM tickets t, clientes c where c.id_clientes = t.id_cliente and t.estatus=".$estatus;
+
+			return ejecutarConsultaSimpleFila($query);
+		}
 	}
 
 ?>
