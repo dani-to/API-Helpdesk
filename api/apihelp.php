4f4df63687bbe4e4badb73f60f491cc21380867c
NayelliSZ <nayellisotoz@gmail.com>
2022-12-08 18:32:38 -0600

    Back
    Api (modificaciones)

--- api/apihelp.php
+++ api/apihelp.php
@@ -11,9 +11,13 @@
 
 			if(count($res)>0){
 				for($row=0; $row<count($res); $row++){
-					$item = array(
+					$item = array(	
 						'folio' => $res[$row][0],
-						'estatus' => $res[$row][1]
+						'id_empleado' => $res[$row][1],
+						'nombre_cliente' => $res[$row][2],
+						'fecha_creacion' => $res[$row][3],
+						'fecha_solucion' => $res[$row][4],
+						'estatus' => ($res[$row][5]==0)? 'PENDIDNTE' : 'REALIZADO'
 					);
 					array_push($tickets['items'], $item);
 				}
--- api/help.php
+++ api/help.php
@@ -4,7 +4,7 @@
 
 	class help{
 		function obtenerTicket(){
-			$query = "SELECT folio, estatus FROM tickets";
+			$query = "SELECT t.folio, t.id_empleado, c.nombre, t.fecha_creacion, t.fecha_solucion, t.estatus FROM tickets t, clientes c where c.id_clientes = t.id_cliente";
 
 			return ejecutarConsultaSimpleFila($query);
 		}
