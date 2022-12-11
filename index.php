6a84bc76954867155f30b0acf7553a3a35c72ab1
NayelliSZ <nayellisotoz@gmail.com>
2022-12-11 16:16:42 -0600

    Back
    api para cesar

--- index.php
+++ index.php
@@ -3,17 +3,17 @@
 
 	$api = new ApiTicket();
 	//echo $_GET;
-	if($_GET["folio"]){
+	if(isset($_GET["folio"])){
 		$api->getId($_GET["folio"]);
-	}else if($_GET["id_empleado"]){
+	}else if(isset($_GET["id_empleado"])){
 		$api->getEmpleado($_GET["id_empleado"]);
-	}else if($_GET["nombre"]){
+	}else if(isset($_GET["nombre"])){
 		$api->getCliente($_GET["nombre"]);
-	}else if($_GET["fecha_creacion"]){
+	}else if(isset($_GET["fecha_creacion"])){
 		$api->getCreacion($_GET["fecha_creacion"]);
-	}else if($_GET["fecha_solucion"]){
+	}else if(isset($_GET["fecha_solucion"])){
 		$api->getSolucion($_GET["fecha_solucion"]);
-	}else if($_GET["estatus"]){
+	}else if(isset($_GET["estatus"])){
 		$api->getEstatus($_GET["estatus"]);
 	}else{
 		$api->getAll();
